1659f866ebb1644faa070fb23f3435b054ed5ac3
aashique03-dev <aaashique03@gmail.com>
2026-02-28 20:51:57 +0500

    sloved errors

--- src/controllers/project.controllers.js
+++ src/controllers/project.controllers.js
@@ -1,8 +1,8 @@
 import { User } from "../models/user.models.js";
 import { Project } from "../models/project.models.js";
 import { ProjectMember } from "../models/projectmember.models.js";
-import { ApiResponse } from "../utils/api-response.js";
-import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+import { ApiError } from "../utils/apiError.js";
 import { asyncHandler } from "../utils/async-handler.js";
 import mongoose from "mongoose";
 import { AvailableUserRole, UserRolesEnum } from "../utils/constants.js";
