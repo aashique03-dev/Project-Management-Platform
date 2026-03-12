d116da7f52623f731b2e4acf0248fb4633c856cf
aashique03-dev <aaashique03@gmail.com>
2026-03-12 14:51:32 +0500

    database from MongoDB Atlas to local Docker MongoDB

--- Server/src/db/index.js
+++ Server/src/db/index.js
@@ -1,3 +1,22 @@
+// new just for test
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`✅ MongoDB connected ${conn.connection.host}`);
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
+
+
+
+/*
+old one orginal
 import mongoose from "mongoose";
 
 const connectDB = async () => {
@@ -10,4 +29,5 @@ const connectDB = async () => {
     }
 }
 
-export default connectDB
+export default connectDB 
+*/
