f001ba6fbe8b4b33ab8a9f6a4aed3820f6d7c0b1
aashique03-dev <aaashique03@gmail.com>
2026-03-17 09:29:44 +0500

    url added

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Change URL correctly if backend is running on a different port or host
-const API_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:8000/api/v1";
+const API_URL = import.meta.env.VITE_API_BASE_URL || "https://project-management-platform-production-6d08.up.railway.app/api/v1";
 
 const api = axios.create({
   baseURL: API_URL,
