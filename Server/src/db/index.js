5a6c251d91211cafafd2eda03395ca32be4e5d06
aashique03-dev <aaashique03@gmail.com>
2026-04-07 12:41:37 +0500

    db added problem sloved

--- Server/src/db/index.js
+++ Server/src/db/index.js
@@ -1,22 +1,3 @@
-// // new just for test
-// import mongoose from "mongoose";
-
-// const connectDB = async () => {
-//   try {
-//     const conn = await mongoose.connect(process.env.MONGODB_URI);
-//     console.log(`✅ MongoDB connected ${conn.connection.host}`);
-//   } catch (error) {
-//     console.error("MongoDB connection error:", error);
-//     process.exit(1);
-//   }
-// };
-
-// export default connectDB;
-
-
-
-
-//old one orginal
 import mongoose from "mongoose";
 
 const connectDB = async () => {
